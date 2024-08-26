5db7e4405d234d08188292cd604a302904182575
Cinnamon <seongsun8330@naver.com>
2024-08-27 01:21:44 +0900

    0827_타일 레퍼런스(마스크) 추가

--- Assets/02_Scripts/PlayerCharacter/Player.cs
+++ Assets/02_Scripts/PlayerCharacter/Player.cs
@@ -26,7 +26,6 @@ namespace PlayerCharacter
         void Update()
         {
             GetPlayerMoveKeyInput();
-            GetPlayerMoveState();
             SetPlayerAnimation();
         }
         //--------------------------------------------------------//
